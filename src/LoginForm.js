afbbaf0dea604f1f84552641ba90c2c3132a064f
aaramiss <31129748+aaramiss@users.noreply.github.com>
2022-05-21 12:57:17 +0430

    removed handleChange from this file

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -4,20 +4,10 @@ import Welcome from './components/Welcome';
 
 const LoginForm = () => {
     
-    const handleChange = e => {
-        return String(e.target.value)
-          .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          );
-    }
-    
     return (
     <div className="container">
         <Welcome />
-        <Login
-        handleChange={handleChange}
-        /> 
+        <Login /> 
     </div>
     )
 };
