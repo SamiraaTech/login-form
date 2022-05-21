c9fb8ce9272f07e19de0967b24cf86fe1a87eac0
aaramiss <31129748+aaramiss@users.noreply.github.com>
2022-05-21 19:04:25 +0430

    validate for input & submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Login = () => {
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
-    // const [ disabled, setDisabled ] = useState(true);
+    const [ disabled, setDisabled ] = useState(true);
 
     const handleSubmit = e => {
-        e.preventDefault();
-        console.log(`${[email]} : ${[password]}`);
+        e.preventDefault(); 
+        let test = String(email)
+        .toLowerCase()
+        .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        if (!test) {
+            alert("Email isn't valid");
+        } else {
+            console.log(email,":",password);
+        }
     }
-    const handleChange = e => {
-        if (String(e.target.value)
-          .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          )) {
-              return true
-          } else {
-              return false
-          } ;
-    }
+    
+    useEffect(() => {
+        if (email && password) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
+    }, [email, password]);
+
     return (
         <div className="login-wrapper">
             <div className="inner-form">
                 <h1>LOGIN</h1>
                 <form className="form" onSubmit={handleSubmit}>
                     <label>Email</label>
-                    <input className="email" type="email" name="email" value={email}
-                     onChange={e => setEmail(e.target.value)} required />
+                    <input className="email disabled" type="text" name="email" value={email}
+                     onChange={e => setEmail(e.target.value)}/>
 
                     <label>Password</label>
-                    <input className="password" type="password" name="password" value={password}
-                    onChange={e => setPassword(e.target.value)} required/>
+                    <input className="password disabled" type="password" name="password" value={password}
+                    onChange={e => setPassword(e.target.value)}/>
 
                     <a href={'/'}>Forget Password?</a>
 
-                    <button className="login_btn" type="submit" value="submit" onClick={handleChange}>Login</button>
+                    <button className="login_btn" type="submit" value="submit" disabled={disabled}>Login</button>
                     <p className='signup'>New to our platform?
                         <a href={'/'}>Signup</a>
                     </p>
@@ -45,11 +50,12 @@ const Login = () => {
                         </div>
 
                     </div>
-                    <div className="social_buttons">
-                        <button className="Fb_btn" type="submit" value="submit" >Facebook</button>
-                        <button className="g_btn" type="submit" value="submit" >Google</button>
-                    </div>
+                    
                 </form>
+                <div className="social_buttons">
+                        <button className="Fb_btn" type="button" value="submit" >Facebook</button>
+                        <button className="g_btn" type="button" value="submit" >Google</button>
+                    </div>
             </div>
         </div>
     )
